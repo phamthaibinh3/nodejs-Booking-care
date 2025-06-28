b331d53f998208e3e3c8d222fad949f000f77fbf
thaibinh <thaibinh100402@gmail.com>
2025-06-28 16:37:34 +0700

    CORS

--- build/server.js
+++ build/server.js
@@ -10,7 +10,7 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default":
 require('dotenv').config();
 var app = (0, _express["default"])();
 app.use((0, _cors["default"])({
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true
 }));
 
